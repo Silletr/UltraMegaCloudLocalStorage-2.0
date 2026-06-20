bd447f78de051975111caf6d46c7e6a01a012c2d
Silletr <silletrMain@gmail.com>
2026-06-20 17:54:10 +0300

    [CHANGED FILE/DIR: .gitignore, src/localcloudstorage/backend.py, src/localcloudstorage/main.py] Wrote first GET request. The rest will be tmrw, I guess [TIMESTAMP: 20/06/2026 | 17:55 UTC+3]

--- src/localcoudstorage/backend.py
+++ src/localcoudstorage/backend.py
@@ -1,17 +1,32 @@
 import asyncpg
 from pydantic import BaseModel
+from fastapi import FastAPI
 
 
 class File(BaseModel):
-    file_name: str
-    file_ext: str  # Will be used in future for sorting
     file_id: int
     file_size: int  # In MB
     file_hash: str
+    file_name: str
+    file_ext: str  # Will be used in future for sorting
+
+
+#  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 
 class FileChunks(BaseModel):
     file_id: int
     chunk_index: int
-    chunk_hash: int
-    chunk_size: int
+    chunk_hash: str
+    chunk_size: int  # PostgreSQL analogue: BIGINT
+
+
+#  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+
+client = FastAPI()
+
+
+@client.get(path="/files-list")
+def get_files_list():
+    return {"hello": "here's empty for now"}
---
+++ src/localcoudstorage/main.py
@@ -0,0 +1,2 @@
+from threading import Thread
+# frontend.py and backend.py is gonna be started via Threads for convenience
