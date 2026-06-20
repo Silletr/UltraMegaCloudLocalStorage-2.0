0e3049cc9da2d0b8d89ff9b56fb53c7cfcbbbff2
Silletr <silletrMain@gmail.com>
2026-06-20 14:54:29 +0300

    [CHANGED FILE/DIR: src/localcloudstorage/backend.py] The last one table is done, code will appear by itself tmrw I guess [TIMESTAMP: 20/06/2026 | 14:55 UTC+3]

--- src/localcoudstorage/backend.py
+++ src/localcoudstorage/backend.py
@@ -8,3 +8,10 @@ class File(BaseModel):
     file_id: int
     file_size: int  # In MB
     file_hash: str
+
+
+class FileChunks(BaseModel):
+    file_id: int
+    chunk_index: int
+    chunk_hash: int
+    chunk_size: int
