76f765fd59ec2e6d27314cac19f8ee1d328459f1
Igor <silletrMain@gmail.com>
2026-06-24 10:50:32 +0300

    [CHANGES FILE/DIR: src/backend.py] Replaced file_size in MB  to file_size_bytes, for coreect calculating chunks in future [CHANGES DID VIA PHONE] [TIMESTAMP: 24/06/2026 | 10:50 UTC+3]

--- src/localcoudstorage/backend.py
+++ src/localcoudstorage/backend.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 class File(BaseModel):
     file_id: int
-    file_size: int  # In MB
+    file_size_bytes: int  # In Bytes
     file_hash: str
     file_name: str
     file_ext: str  # Will be used in future for sorting
